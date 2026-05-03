bd61e234b88a1d015d2aa1f3b018d7f7b99abd8b
Lunar-Dawn <lunarrequiem42@gmail.com>
2026-05-04 01:48:41 +0200

    Reorganize the skill fields

--- src/model/character.mjs
+++ src/model/character.mjs
@@ -12,6 +12,22 @@ export class CharacterData extends BaseCharacterData {
 	static LOCALIZATION_PREFIXES = ["warden.character"];
 
 	static defineSchema() {
+		const proficiencyField = () =>
+			new SchemaField({
+				rank: new NumberField({
+					required: true,
+					integer: true,
+					min: 0,
+					max: 5,
+					initial: 0,
+				}),
+			});
+
+		const skillField = () =>
+			new SchemaField({
+				proficient: new BooleanField({ required: true }),
+			});
+
 		return {
 			...super.defineSchema(),
 
@@ -53,81 +69,30 @@ export class CharacterData extends BaseCharacterData {
 			has_savings: new BooleanField({ required: true }),
 
 			path: new SchemaField({
-				combat: new SchemaField({
-					rank: new NumberField({
-						required: true,
-						integer: true,
-						min: 0,
-						max: 5,
-						initial: 0,
-					}),
-				}),
-				skill: new SchemaField({
-					rank: new NumberField({
-						required: true,
-						integer: true,
-						min: 0,
-						max: 5,
-						initial: 0,
-					}),
-				}),
-				special: new SchemaField({
-					rank: new NumberField({
-						required: true,
-						integer: true,
-						min: 0,
-						max: 5,
-						initial: 0,
-					}),
-				}),
+				combat: proficiencyField(),
+				skill: proficiencyField(),
+				special: proficiencyField(),
 			}),
 
 			defense: new SchemaField({
-				toughness: new SchemaField({
-					rank: new NumberField({
-						required: true,
-						integer: true,
-						min: 0,
-						max: 5,
-						initial: 0,
-					}),
-				}),
-				resolve: new SchemaField({
-					rank: new NumberField({
-						required: true,
-						integer: true,
-						min: 0,
-						max: 5,
-						initial: 0,
-					}),
-				}),
-				perception: new SchemaField({
-					rank: new NumberField({
-						required: true,
-						integer: true,
-						min: 0,
-						max: 5,
-						initial: 0,
-					}),
-				}),
+				toughness: proficiencyField(),
+				resolve: proficiencyField(),
+				perception: proficiencyField(),
 			}),
 
-			skill_proficiency: new SchemaField({
-				crafting: new BooleanField({ required: true }),
-				deception: new BooleanField({ required: true }),
-				diplomacy: new BooleanField({ required: true }),
-				force: new BooleanField({ required: true }),
-				intimidation: new BooleanField({
-					required: true,
-				}),
-				medicine: new BooleanField({ required: true }),
-				mobility: new BooleanField({ required: true }),
-				skullduggery: new BooleanField({
-					required: true,
-				}),
-				stealth: new BooleanField({ required: true }),
-				survival: new BooleanField({ required: true }),
+			skill: new SchemaField({
+				crafting: skillField(),
+				deception: skillField(),
+				diplomacy: skillField(),
+				force: skillField(),
+				intimidation: skillField(),
+				medicine: skillField(),
+				mobility: skillField(),
+				skullduggery: skillField(),
+				stealth: skillField(),
+				survival: skillField(),
 			}),
+
 			knowledge_skills: new TypedObjectField(
 				new SchemaField({
 					is_niche: new BooleanField({
