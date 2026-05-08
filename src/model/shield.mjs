acc688e07063b8f201f0c2e340feb9423e7bc3b9
Lunar-Dawn <lunarrequiem42@gmail.com>
2026-05-08 08:41:13 +0200

    Add properties to Shield Model

--- src/model/shield.mjs
+++ src/model/shield.mjs
@@ -1,3 +1,46 @@
 import { BaseEquipment } from "./base_equipment.mjs";
 
-export class Shield extends BaseEquipment {}
+const { NumberField } = foundry.data.fields;
+
+/**
+ * @property {number} defense
+ * @property {number} block
+ */
+export class Shield extends BaseEquipment {
+	static defineSchema() {
+		return {
+			...super.defineSchema(),
+
+			defense: new NumberField({
+				required: true,
+				initial: 0,
+				min: 0,
+				integer: true,
+				label: "warden.shield.defense.label",
+			}),
+
+			block: new NumberField({
+				required: true,
+				initial: 0,
+				min: 0,
+				integer: true,
+				label: "warden.shield.block.label",
+			}),
+		};
+	}
+
+	getProperties() {
+		const properties = { ...super.getProperties() };
+
+		properties.defense = {
+			field: this.schema.fields.defense,
+			value: this.defense,
+		};
+		properties.block = {
+			field: this.schema.fields.block,
+			value: this.block,
+		};
+
+		return properties;
+	}
+}
